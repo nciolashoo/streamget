a71262fe27f8435f7738e58ba649fdcd9f7dcb5b
NicolasHoo <nedvedheqing@gmail.com>
2025-08-21 20:38:54 +0800

    支持京东直播的循环监控，支持通过app分享的直播账号主页地址、直播间当前地址和PC端网页直播间地址，修复了不能正常获取直播账号名称的bug

--- streamget/platforms/jd/live_stream.py
+++ streamget/platforms/jd/live_stream.py
@@ -35,29 +35,56 @@ class JDLiveStream(BaseLiveStream):
         Returns:
             dict: A dictionary containing anchor name, live status, room URL, and title.
         """
-        redirect_url = await async_req(url, proxy_addr=self.proxy_addr, headers=self.mobile_headers, redirect_url=True)
-        author_id = self.get_params(redirect_url, 'authorId')
         result = {"anchor_name": '', "is_live": False}
+        
+        url = await async_req(url, proxy_addr=self.proxy_addr, headers=self.mobile_headers, redirect_url=True)
+        #尝试直接在链接里获取参数authorId         
+        author_id=self.get_params(url,'authorId')
         if not author_id:
-            live_id = re.search('#/(.*?)\\?origin', redirect_url)
+            #没能通过链接获取参数authorId的情况下，尝试直接在链接里获取参数liveId
+            live_id = self.get_params(url, 'liveId')
             if not live_id:
-                return result
-            live_id = live_id.group(1)
-            result['anchor_name'] = f'jd_{live_id}'
-        else:
-            data = {
+                #没能通过链接获取参数authorId或liveId的情况下，通过正则匹配寻找liveId
+                live_id_matchs = re.search('#/(.*?)\\?origin', url)
+                if live_id_matchs:
+                    live_id = live_id_matchs.group(1)
+            
+            if not live_id:
+                #尝试所有途径获取liveId失败，无法通过liveId获取authorId，抛出异常
+                raise Exception('Error: Invalid URL: missing required parameters "authorId" or "liveId"')
+            
+            params = {
+                'functionId': 'liveBasicDetailToM',
+                'appid': 'live_pc',
+                'body': '{"liveId":"' + live_id + '"}'
+            }
+            info_api = f'https://api.m.jd.com/api?{urllib.parse.urlencode(params)}'
+            json_str = await async_req(info_api,proxy_addr=self.proxy_addr, headers=self.mobile_headers)
+            json_data = json.loads(json_str)
+            if 'data' in json_data and 'authorInfo' in json_data['data']:
+                author_id=json_data['data']['authorInfo']['authorId']
+                
+            if not author_id:
+                #尝试所有途径获取authorId失败，抛出异常
+                raise Exception('Error: The live room URL has expired. Please use the URL of the currently active live stream.')
+                                
+        
+        data = {
                 'functionId': 'talent_head_findTalentMsg',
                 'appid': 'dr_detail',
                 'body': '{"authorId":"' + author_id + '","monitorSource":"1","userId":""}',
             }
-            info_api = 'https://api.m.jd.com/talent_head_findTalentMsg'
-            json_str = await async_req(info_api, data=data, proxy_addr=self.proxy_addr, headers=self.mobile_headers)
-            json_data = json.loads(json_str)
-            anchor_name = json_data['result']['talentName']
-            result['anchor_name'] = anchor_name
-            if 'livingRoomJump' not in json_data['result']:
-                return result
-            live_id = json_data['result']['livingRoomJump']['params']['id']
+        info_api = 'https://api.m.jd.com/talent_head_findTalentMsg'
+        json_str = await async_req(info_api, data=data, proxy_addr=self.proxy_addr, headers=self.mobile_headers)
+        json_data = json.loads(json_str)
+        anchor_name = json_data['result']['talentName']
+        result['anchor_name'] = anchor_name
+        if 'livingRoomJump' not in json_data['result']:
+            return result
+        #通过authorId获取当前live_id
+        live_id = json_data['result']['livingRoomJump']['params']['id']
+        
+                
         params = {
             "body": '{"liveId": "' + live_id + '"}',
             "functionId": "getImmediatePlayToM",
@@ -70,21 +97,9 @@ class JDLiveStream(BaseLiveStream):
         json_data = json.loads(json_str)
         live_status = json_data['data']['status']
         if live_status == 1:
-            if author_id:
-                data = {
-                    'functionId': 'jdTalentContentList',
-                    'appid': 'dr_detail',
-                    'body': '{"authorId":"' + author_id + '","type":1,"userId":"","page":1,"offset":"-1",'
-                                                          '"monitorSource":"1","pageSize":1}',
-                }
-                json_str2 = await async_req('https://api.m.jd.com/jdTalentContentList', data=data,
-                                            proxy_addr=self.proxy_addr, headers=self.mobile_headers)
-                json_data2 = json.loads(json_str2)
-                result['title'] = json_data2['result']['content'][0]['title']
-
             flv_url = json_data['data']['videoUrl']
             m3u8_url = json_data['data']['h5VideoUrl']
-            result |= {"is_live": True, "m3u8_url": m3u8_url, "flv_url": flv_url, "record_url": m3u8_url}
+            result |= {"is_live": True, "m3u8_url": m3u8_url, "flv_url": flv_url, "record_url": flv_url}
         return result
 
     @staticmethod
